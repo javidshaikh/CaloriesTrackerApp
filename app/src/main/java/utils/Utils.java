16085b3bc3f9bd8b83bb0ed4a55a853759f33457
javidshaikh <javidkoh@gmail.com>
2016-08-23 23:55:03 +0530

    Added

---
+++ app/src/main/java/utils/Utils.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by paulodichone on 2/28/15.
+ */
+public class Utils {
+
+
+    public static String formatNumber( int value) {
+        DecimalFormat formatter = new DecimalFormat("#,###,###");
+        String formatted = formatter.format(value);
+
+        return formatted;
+    }
+}
